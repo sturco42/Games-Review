4c435ecae763af061000e48ca4c020061c2b1526
Jay Lim <jlimoman@gmail.com>
2023-06-05 11:55:48 -0600

    initial definition of customer class

--- lib/classes/customers.py
+++ lib/classes/customers.py
@@ -2,5 +2,5 @@ class Customers:
   
   def __init__(self, first_name, last_name, games_list):
     self.first_name = first_name
-    self.last_nmae = last_name
+    self.last_name = last_name
     self.games_list = games_list
