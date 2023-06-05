e20826f9cb80cc8cd7dfe4c612f7f1bb6dcc31b0
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-06-05 14:29:55 -0700

    has the limitation for the charactors

--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -1,5 +1,6 @@
 class Customer:
     all = []
+    username_list = []
     def __init__(self, first_name, last_name, username) -> None:
         self.first_name = first_name
         self.last_name = last_name
@@ -12,7 +13,7 @@ class Customer:
     
     @first_name.setter
     def first_name(self, new_first_name):
-        if isinstance(new_first_name, str) and 1 <= len(new_first_name):
+        if isinstance(new_first_name, str) and 1 <= len(new_first_name) <=30:
             self._first_name = new_first_name
         else:
             raise TypeError('First name must be str and cannot be blank!')
@@ -23,7 +24,7 @@ class Customer:
     
     @last_name.setter
     def last_name(self, new_last_name):
-        if isinstance(new_last_name, str) and 1 <= len(new_last_name):
+        if isinstance(new_last_name, str) and 1 <= len(new_last_name) <=30:
             self._last_name = new_last_name
         else:
             raise TypeError('Last name must be str and cannot be blank!')
@@ -34,10 +35,12 @@ class Customer:
     
     @username.setter
     def username(self, new_username):
-        username_list = [customer.username for customer in type(self).all]
-        if new_username not in username_list:
-            username_list.append(new_username)
-            self._username = new_username
+        if new_username not in type(self).username_list:
+            if 1 >= len(new_username) or len(new_username) >= 30:
+                raise TypeError('Please make sure your username is between 1 to 30 characters.')
+            else: 
+                type(self).username_list.append(new_username)
+                self._username = new_username          
         else:
             raise TypeError('This username has been taken, please choose another one.')
             
--- lib/debug.py
+++ lib/debug.py
@@ -12,7 +12,8 @@ from classes.review import Review
 #Customer
 c1 = Customer('John', 'Smith', 'Gamer NO.1')
 # c2 = Customer(1, 'Smith', 'adjhj') => TypeError: First name cannot be blank or number!
-# c3 = Customer('John', 'Smith', 'Gamer NO.1') => TypeError: This username has been taken, please choose another one.
+# c3 = Customer('John', 'Smith', 'Gamer NO.1') #=> TypeError: This username has been taken, please choose another one.
+# c4 = Customer('John', 'Smith', 's') #=> TypeError: This username has been taken, please choose another one.
 
 #Transaction
 # t1 = Transaction(g1, c1)
