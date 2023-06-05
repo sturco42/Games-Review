91587400fdf42303d1423692dd8e89ffa9fa03bf
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-06-05 14:40:54 -0700

    consistent requirements for names

--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -16,7 +16,7 @@ class Customer:
         if isinstance(new_first_name, str) and 1 <= len(new_first_name) <=30:
             self._first_name = new_first_name
         else:
-            raise TypeError('First name must be str and cannot be blank!')
+            raise TypeError('The first name must be a string between 1 to 30 characters.')
     
     @property
     def last_name(self):
@@ -27,7 +27,7 @@ class Customer:
         if isinstance(new_last_name, str) and 1 <= len(new_last_name) <=30:
             self._last_name = new_last_name
         else:
-            raise TypeError('Last name must be str and cannot be blank!')
+            raise TypeError('The last name must be a string between 1 to 30 characters.')
     
     @property
     def username(self):
@@ -37,7 +37,7 @@ class Customer:
     def username(self, new_username):
         if new_username not in type(self).username_list:
             if 1 >= len(new_username) or len(new_username) >= 30:
-                raise TypeError('Please make sure your username is between 1 to 30 characters.')
+                raise TypeError('The username must be a string between 1 to 30 characters.')
             else: 
                 type(self).username_list.append(new_username)
                 self._username = new_username          
